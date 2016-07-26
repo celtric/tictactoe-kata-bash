02dc11e7f5aa9dc6cac135ab5bbc1fc6c0641b42
Ricard Osorio <celtric@gmail.com>
2016-07-26 19:36:49 +0200

    accept first command

--- src/tictactoe.sh
+++ src/tictactoe.sh
@@ -1,9 +1,56 @@
-echo ""
-echo "+---+---+---+"
-echo "|   |   |   |"
-echo "+---+---+---+"
-echo "|   |   |   |"
-echo "+---+---+---+"
-echo "|   |   |   |"
-echo "+---+---+---+"
-echo ""
+#!/usr/bin/env bash
+
+declare -a row0=(" " " " " ")
+declare -a row1=(" " " " " ")
+declare -a row2=(" " " " " ")
+
+#---[ Helpers ]----------------------------------------------------------------#
+
+RED='\033[1;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
+#---[ Functions ]--------------------------------------------------------------#
+
+bye() {
+    echo -e "\n${RED}Farewell, Professor Falken.$RESET"
+    exit 0
+}
+
+printBoard() {
+    echo -e "\033c"
+    echo "+---+---+---+"
+    echo "| ${row0[0]} | ${row0[1]} | ${row0[2]} |"
+    echo "+---+---+---+"
+    echo "| ${row1[0]} | ${row1[1]} | ${row1[2]} |"
+    echo "+---+---+---+"
+    echo "| ${row2[0]} | ${row2[1]} | ${row2[2]} |"
+    echo "+---+---+---+"
+}
+
+handleCommand() {
+    x=${1:0:1}
+    y=${1:1:2}
+    case $x in
+        0) row0[y]="X";;
+        1) row1[y]="X";;
+        2) row2[y]="X";;
+    esac
+    printBoard
+    bye
+}
+
+#---[ Main ]-------------------------------------------------------------------#
+
+printBoard
+
+handleCommand $1
+
+while true; do
+    read -p "Player A, what's your move (x y)? " xy
+    case $xy in
+        "exit") bye
+        *) printBoard
+    esac
+done
--- tests/tictactoe-tests.sh
+++ tests/tictactoe-tests.sh
@@ -14,3 +14,14 @@ assert_contains "src/tictactoe.sh" "
 +---+---+---+
 "
 assert_end prints_empty_board
+
+assert_contains "src/tictactoe.sh 00" "
++---+---+---+
+| X |   |   |
++---+---+---+
+|   |   |   |
++---+---+---+
+|   |   |   |
++---+---+---+
+"
+assert_end accepts_input
